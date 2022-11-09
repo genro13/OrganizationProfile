bb5e13bc32439926383d721d4717140f63ebc7cc
CLAB1- <CLAB1-@DESKTOP-1EFPVBO>
2022-11-09 11:25:59 +0800

    Add project files.

---
+++ OrganizationProfile/StudentInformationClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrganizationProfile
+{
+    class StudentInformationClass
+    {
+        public static int setStudentNo = 0;
+        public static string setContactNo;
+        public static int setAge = 0;
+        public static string setProgram = "";
+        public static string setGender = "";
+        public static string setBirthday = "";
+        public static string setFullName = "";
+    }
+}
